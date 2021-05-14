f2b6ca86ad080d61b1c425e16e855cb8883082fa
Sumit Maheshwari <sumit@astronomer.io>
2021-05-14 13:03:00 +0530

    Fix version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools import setup, find_packages  # noqa: H301
 
 
 NAME = "apache-airflow-client"
-VERSION = "2.0.0"
+VERSION = "2.0.1"
 # To install the library, run the following
 #
 # python setup.py install

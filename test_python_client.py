11726ed156d1dff578a6dfffae2e444f9460036d
Jarek Potiuk <jarek@potiuk.com>
2024-01-14 08:56:01 +0100

    Modernize Python Client packaging (#104)
    Accompanying https://github.com/apache/airflow/pull/36763 where we
    fix and modernize the way how Python client gets generated, this
    on is result of applying the modernisation:
    
    * converting project configuration to pyproject.toml and removing
      all setup.* and requirements files
    * converting to modern packaging backend (hatchling) to build
      wheel and sdist packages (with reproducibility)
    * using hatch test environment definition and coverage to run tests
    * we deleted release instructions and dev tools (the ones in Airflow
      will be used to generate and sign the package)
    * similarly .pre-comit-config.yml is not needed as the client
      gets generated in the Apache Airflow project.
    
    All those are going to be maintained in Apache Airflow project
    as part of https://github.com/apache/airflow/pull/36763 as a single
    source of truth - when new Python client gets released the project
    files will be overwritten from those prepare from Airflow project,
    this is just initial PR to seed the new

--- test_python_client.py
+++ test_python_client.py
@@ -14,9 +14,22 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+#
+# PEP 723 compliant inline script metadata (not yet widely supported)
+# /// script
+# requires-python = ">=3.8"
+# dependencies = [
+#   "apache-airflow-client",
+#   "rich",
+# ]
+# ///
+
+from __future__ import annotations
+
 import uuid
 
 import airflow_client.client
+
 try:
     # If you have rich installed, you will have nice colored output of the API responses
     from rich import print
@@ -42,9 +55,7 @@ from airflow_client.client.model.dag_run import DAGRun
 
 # Configure HTTP basic authorization: Basic
 configuration = airflow_client.client.Configuration(
-    host="http://localhost:8080/api/v1",
-    username='admin',
-    password='admin'
+    host="http://localhost:8080/api/v1", username="admin", password="admin"
 )
 
 # Make sure in the [core] section, the  `load_examples` config is set to True in your airflow.cfg
@@ -53,47 +64,44 @@ DAG_ID = "example_bash_operator"
 
 # Enter a context with an instance of the API client
 with airflow_client.client.ApiClient(configuration) as api_client:
-
     errors = False
 
-    print('[blue]Getting DAG list')
+    print("[blue]Getting DAG list")
     dag_api_instance = dag_api.DAGApi(api_client)
     try:
         api_response = dag_api_instance.get_dags()
         print(api_response)
     except airflow_client.client.OpenApiException as e:
-        print("[red]Exception when calling DagAPI->get_dags: %s\n" % e)
+        print(f"[red]Exception when calling DagAPI->get_dags: {e}\n")
         errors = True
     else:
-        print('[green]Getting DAG list successful')
+        print("[green]Getting DAG list successful")
 
-
-    print('[blue]Getting Tasks for a DAG')
+    print("[blue]Getting Tasks for a DAG")
     try:
         api_response = dag_api_instance.get_tasks(DAG_ID)
         print(api_response)
     except airflow_client.client.exceptions.OpenApiException as e:
-        print("[red]Exception when calling DagAPI->get_tasks: %s\n" % e)
+        print(f"[red]Exception when calling DagAPI->get_tasks: {e}\n")
         errors = True
     else:
-        print('[green]Getting Tasks successful')
+        print("[green]Getting Tasks successful")
 
-
-    print('[blue]Triggering a DAG run')
+    print("[blue]Triggering a DAG run")
     dag_run_api_instance = dag_run_api.DAGRunApi(api_client)
     try:
         # Create a DAGRun object (no dag_id should be specified because it is read-only property of DAGRun)
         # dag_run id is generated randomly to allow multiple executions of the script
         dag_run = DAGRun(
-            dag_run_id='some_test_run_' + uuid.uuid4().hex,
+            dag_run_id="some_test_run_" + uuid.uuid4().hex,
         )
         api_response = dag_run_api_instance.post_dag_run(DAG_ID, dag_run)
         print(api_response)
     except airflow_client.client.exceptions.OpenApiException as e:
-        print("[red]Exception when calling DAGRunAPI->post_dag_run: %s\n" % e)
+        print(f"[red]Exception when calling DAGRunAPI->post_dag_run: {e}\n")
         errors = True
     else:
-        print('[green]Posting DAG Run successful')
+        print("[green]Posting DAG Run successful")
 
     # Get current configuration. Note, this is disabled by default with most installation.
     # You need to set `expose_config = True` in Airflow configuration in order to retrieve configuration.
@@ -102,12 +110,19 @@ with airflow_client.client.ApiClient(configuration) as api_client:
         api_response = conf_api_instance.get_config()
         print(api_response)
     except airflow_client.client.OpenApiException as e:
-        print("[red]Exception when calling ConfigApi->get_config: %s\n" % e)
-        errors = True
+        if "FORBIDDEN" in str(e):
+            print(
+                "[yellow]You need to set `expose_config = True` in Airflow configuration"
+                " in order to retrieve configuration."
+            )
+            print("[bright_blue]This is OK. Exposing config is disabled by default.")
+        else:
+            print(f"[red]Exception when calling DAGRunAPI->post_dag_run: {e}\n")
+            errors = True
     else:
-        print('[green]Config retrieved successfully')
+        print("[green]Config retrieved successfully")
 
     if errors:
-        print ('\n[red]There were errors while running the script - see above for details')
+        print("\n[red]There were errors while running the script - see above for details")
     else:
-        print ('\n[green]Everything went well')
+        print("\n[green]Everything went well")

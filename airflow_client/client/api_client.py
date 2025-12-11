f8859b08bb72b2cbf5d78189155fe910971a7fd5
Ephraim Anierobi <splendidzigy24@gmail.com>
2025-12-11 17:44:19 +0100

    Update Python Client to 3.1.4rc1 (#141)

--- airflow_client/client/api_client.py
+++ airflow_client/client/api_client.py
@@ -90,7 +90,7 @@ class ApiClient:
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'OpenAPI-Generator/3.1.3/python'
+        self.user_agent = 'OpenAPI-Generator/3.1.4/python'
         self.client_side_validation = configuration.client_side_validation
 
     def __enter__(self):
